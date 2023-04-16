643cd92ffa05dc9d6017d069c6bec88c86b8d19d
Shannon Gold <shannongold@gmail.com>
2023-04-16 15:11:54 -0700

    Second commit. Nearly done.

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
 def home(request):
-	return render(request, 'home.html', {})
+
+	if request.method == 'POST':
+		form = ListForm(request.POST or None)
+
+		if form.is_valid():
+			form.save()
+			all_items = List.objects.all
+			messages.success(request, ('A new tem has been added to the List.'))
+			return render(request, 'home.html', {'all_items' : all_items})
+	else:
+		all_items = List.objects.all
+		return render(request, 'home.html', {'all_items' : all_items})
+
+def about(request):
+	return render(request, 'about.html', {})
+
+def delete(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.delete()
+	messages.success(request, ('Item has been deleted.'))
+	return redirect('home')
+
