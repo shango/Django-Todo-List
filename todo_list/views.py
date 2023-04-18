3e879c064504ab40aaa174d5a42417a674778d6d
Shannon Gold <shannongold@gmail.com>
2023-04-18 15:29:47 -0700

    added cross and uncross buttons

--- todo_list/views.py
+++ todo_list/views.py
@@ -29,3 +29,29 @@ def delete(request, list_id):
 	messages.success(request, ('Item has been deleted.'))
 	return redirect('home')
 
+def cross_off(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = True
+	item.save()
+	messages.success(request, ('Item has been crossed off.'))
+	return redirect('home')
+
+def uncross(request, list_id):
+	item = List.objects.get(pk=list_id)
+	item.completed = False
+	item.save()
+	messages.success(request, ('Item has been uncrossed off.'))
+	return redirect('home')
+
+def edit(request, list_id):
+	if request.method == 'POST':
+		form = ListForm(request.POST or None)
+
+		if form.is_valid():
+			form.save()
+			all_items = List.objects.all
+			messages.success(request, ('A new tem has been edited.'))
+			return render(request, 'home.html', {'all_items' : all_items})
+	else:
+		item = List.objects.get(pk=list_id)
+		return render(request, 'home.html', {'item' : item})
